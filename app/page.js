cf31cbd3664d86b7c8b0f0e63baa5bced27bb779
anupam-kumar-krishnan <anupamk.krishnan@gmail.com>
2025-06-03 23:21:43 +0530

    ft :: initial setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>AI-Note Taking</h2>
+      <Button>Get Started</Button>
+    </div>
+  );
+}
