c4553c9e7a07ce9fde726ae768b2c9b27c1ee22d
anupam-kumar-krishnan <anupamk.krishnan@gmail.com>
2025-06-04 00:26:05 +0530

    ft :: clerk authentication

--- app/provider.js
+++ app/provider.js
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 function Provider({ children }) {
   const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
